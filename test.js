80a697cef3b633f8c4d35062c3f6b20eb4dff3f8
mrzmmr <mrzmmr@gmail.com>
2019-07-19 17:39:09 -0700

    Add tests

--- test.js
+++ test.js
@@ -1,29 +1,21 @@
-var unified = require('unified')
-var parser = require('remark-parse')
-var compiler = require('remark-stringify')
+var remark = require('remark')
 var test = require('tap').test
 var variables = require('.')
 var match = require('./lib/match')
 
 test('variables', function (t) {
   t.doesNotThrow(function () {
-    unified()
+    remark()
       .use(variables)
       .freeze()
   }, 'should not throw without parser')
 
   t.doesNotThrow(function () {
-    t.ok(unified()
-      .use(parser)
-      .use(compiler)
-      .use(variables)
-    )
+    t.ok(remark().use(variables))
   }, 'should not throw without options')
 
   t.doesNotThrow(function() {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables)
       .processSync('{{ foo }}')
 
@@ -31,9 +23,7 @@ test('variables', function (t) {
   }, 'should not throw when no data is found')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables)
       .data('foo', 'bar')
       .processSync('{{ foo }}')
@@ -41,9 +31,7 @@ test('variables', function (t) {
   }, 'should not throw with default options')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, ':')
       .data('foo', 'bar')
       .processSync(': foo :')
@@ -51,9 +39,7 @@ test('variables', function (t) {
   }, 'should not throw with custom fence as string')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, [':'])
       .data('foo', 'bar')
       .processSync(': foo :')
@@ -62,9 +48,7 @@ test('variables', function (t) {
   }, 'should not throw with custom fence as an array.length = 1')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, ['->', '<-'])
       .data('foo', 'bar')
       .processSync('-> foo <-')
@@ -73,9 +57,7 @@ test('variables', function (t) {
   }, 'should not throw with custom fence as an array.length = 2')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, [false])
       .processSync('foo')
 
@@ -83,9 +65,7 @@ test('variables', function (t) {
   }, 'should not throw and should resort to defaults with bad custom fence')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, { fence: ':' })
       .data('foo', 'bar')
       .processSync(': foo :')
@@ -94,9 +74,7 @@ test('variables', function (t) {
   }, 'should not throw with custom fence as an object')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, { fence: { open: '{{', close: '}}' } })
       .data('foo', 'bar')
       .processSync('{{ foo }}')
@@ -105,9 +83,7 @@ test('variables', function (t) {
   }, 'should not throw with custom fence as an object; open, close')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, { fence: { open: ':' } })
       .data('foo', 'bar')
       .processSync(': foo :')
@@ -116,9 +92,7 @@ test('variables', function (t) {
   }, 'should not throw with custom fence as an object; open')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, { name: 'myVariables' })
       .data('foo', 'bar')
       .processSync('{{ foo }}')
@@ -126,23 +100,8 @@ test('variables', function (t) {
     t.ok(file.toString() === 'bar\n')
   }, 'should not throw with custom node type name')
 
-  /*
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
-      .use(variables, { test: /\:\:(.*?)\:\:/ })
-      .data('foo', 'bar')
-      .processSync('::foo::')
-
-    t.ok(file.toString() === 'bar\n')
-  }, 'should not throw with using test regex')
-  */
-
-  t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables)
       .use(function () {
         return function (tree, file) {
@@ -155,18 +114,14 @@ test('variables', function (t) {
   }, 'should not throw with data from transform stage')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables)
       .data('foo', 0)
       .processSync('{{ .foo }}')
 
     t.ok(file.toString() === '0\n')
 
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables)
       .data('foo', false)
       .processSync('{{ foo }}')
@@ -175,17 +130,13 @@ test('variables', function (t) {
   }, 'should not throw and match when data value is zero or false')
 
   t.throws(function () {
-    unified()
-      .use(parser)
-      .use(compiler)
+    remark()
       .use(variables, { fail: true })
       .processSync('{{ foo }}')
   })
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables)
       .processSync('{{ .foo }}')
 
@@ -193,14 +144,37 @@ test('variables', function (t) {
   }, 'should not throw and should create message')
 
   t.doesNotThrow(function () {
-    var file = unified()
-      .use(parser)
-      .use(compiler)
+    var file = remark()
       .use(variables, { quiet: true })
       .processSync('{{ .foo }}')
 
     t.ok(file.messages.length === 0)
   }, 'should not throw and no messages with quiet option')
+
+  t.doesNotThrow(function () {
+    remark()
+      .use(function () {
+        this.Parser = function () {
+          return {
+            type: 'root',
+            children: []
+          }
+        }
+      })
+      .use(variables)
+      .processSync('{{ .foo }}')
+  }, 'should do nothing if is not a remark parser.')
+
+  t.doesNotThrow(function () {
+    remark()
+      .use(function () {
+        this.Compiler = function () {
+          return ''
+        }
+      })
+      .use(variables)
+      .processSync('{{ .foo }}')
+  }, 'should do nothing if is not a remark compiler.')
 
   t.end()
 })
